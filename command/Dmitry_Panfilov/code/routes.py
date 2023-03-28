3b19a2377ad59903c490870e8a546407544f3a95
Dmitry <99917230+dnp34@users.noreply.github.com>
2023-03-28 15:31:32 +0300

    Update routes.py

--- command/Dmitry_Panfilov/code/routes.py
+++ command/Dmitry_Panfilov/code/routes.py
@@ -1,3 +1,7 @@
+# определение API-маршрута для обработки изображений, 
+# используя FastAPI для запуска сервера на локальном хосте и порту 8000
+# уровень логирования установлен на отладочный (debug), чтобы отслеживать ошибки в коде
+# автоматическая перезагрузка при изменении кода (reload=True) упрощает разработку и тестирование приложения
 from fastapi import APIRouter, HTTPException
 from schemas import *
 from config import TELEGRAM_USERS
@@ -5,6 +9,10 @@ from utils import imager
 
 mrt = APIRouter()
 
+# API-маршрут принимает POST-запросы по адресу "/image_processing" и принимает объект 
+# типа ClientMessage, содержащий информацию о клиенте и URL изображения для обработки
+# Если тип клиента не указан, он устанавливается на "web", а дальше
+# в зависимости от типа клиента, вызывается функция "imager", которая обрабатывает изображение по заданному URL
 @mrt.post('/image_processing', tags=["test"])
 async def image_processing(client_message: ClientMessage) -> RespSchema:
     if client_message.client is None:
@@ -13,6 +21,10 @@ async def image_processing(client_message: ClientMessage) -> RespSchema:
     client = client_message.client.lower()
     img_url = client_message.image_url
     
+    # если тип клиента - "bot", то проверяется наличие chat_id в запросе и проверяется, 
+    # что он принадлежит списку разрешенных TELEGRAM_USERS;
+    # если chat_id не указан или не принадлежит списку разрешенных пользователей, 
+    # то возвращается ошибка HTTPException с кодом 400 или 403
     if client == 'bot':
         if client_message.chat_id is None:
             raise HTTPException(status_code=400, 
@@ -24,6 +36,9 @@ async def image_processing(client_message: ClientMessage) -> RespSchema:
             raise HTTPException(status_code=403, 
                                 detail='Доступ ЗАПРЕЩЕН!')
         ans = imager(img_url)
+    
+    # Если тип клиента - "web" или "android", то функция "imager" просто возвращает 
+    # сегментированное изображение без дополнительной проверки
     elif client == 'web':
         ans = imager(img_url)
     elif client == 'android':
@@ -31,9 +46,10 @@ async def image_processing(client_message: ClientMessage) -> RespSchema:
     else:
         raise HTTPException(status_code=410, 
                             detail='Неизвестный Тип Данных!')
-
+    # если тип клиента неизвестен, то возвращается ошибка HTTPException с кодом 410
     if ans.code != 200:
         raise HTTPException(status_code=ans.code, 
-                    detail=f'Ошибка: {ans.message}')
+                    detail=f'Ошибка: {ans.message}')  # возвращается соответствующая ошибка HTTPException
     
+    # если обработка изображения прошла успешно, то возвращается объект из RespSchema
     return RespSchema(**ans)
