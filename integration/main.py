1be1b1edd45fcbaf46026bae8795dcde47aa11e5
zhev <zhevnerov@gmail.com>
2023-04-25 18:08:41 +0300

    Update main.py
    The function segment_file has been added.

--- integration/main.py
+++ integration/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+# Запустить сервер:
+# 
+# uvicorn main:app --reload
+#
+
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from api_tools import router as api_router
+
+from PIL import Image, ImageFilter
+import uvicorn
+import io
+import tempfile
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -14,3 +24,40 @@ async def read_root(request: Request):
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+  
+def process_file(image):
+    # обработку изображения
+    # например, размытие
+    image = image.filter(ImageFilter.BLUR)
+    return image
+    
+
+@app.post("/file/segment-file")
+async def segment_file(file: UploadFile = File(...)):
+    print('Временная директория: ', tempfile.gettempdir())
+    image = Image.open(file.file)
+    
+    # обработка изображения
+    image = process_file(image)
+    
+    # создаем временный файл
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        # сохраняем измененную картинку во временный файл
+        image.save(tmp, format='JPEG')
+        
+    # возвращаем временный файл как ответ на запрос
+    res = FileResponse(tmp.name, filename="processed_" + file.filename)
+    
+    # удаляем временный файл после ответа на запрос
+#    def remove_file():
+#        try:
+#            shutil.rmtree(tmp.name)
+#        except:
+#            pass
+#    res.background(remove_file)
+    
+    return res  
+  
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
